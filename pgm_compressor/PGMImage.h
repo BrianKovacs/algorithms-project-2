56f6ba7044001e7b6b47a803348d5e1ae4d7a153
Brian Kovacs <brian.kovacs@me.com>
2018-03-31 15:18:02 -0400

    Open and save to ASCII pgm format

--- pgm_compressor/PGMImage.h
+++ pgm_compressor/PGMImage.h
@@ -9,12 +9,120 @@
 #ifndef PGMImage_h
 #define PGMImage_h
 
-class PGMImage {
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::getline;
+using std::cout;
+using std::endl;
+
+class PGMImage
+{
 public:
     int width;
     int height;
     int max;
     int* arr;
+    
+    PGMImage() {width=0; height=0; max=0;};
+    int getSize() {return width*height;}
+    
+    bool loadASCII(const char* file);
+    bool saveASCII(const char* file);
+    bool loadBinary(const char* file);
+    bool saveBinary(const char* file);
 };
+
+bool PGMImage::loadASCII(const char* file)
+{
+    string line;
+    ifstream infile(file);
+    
+    if (!infile) {
+        cout << "File does not exist!\n";
+        return false;
+    }
+    
+    int n = 0;
+    int i = 0;
+    int size = 0;
+    while (getline(infile, line))
+    {
+        std::istringstream iss(line);
+        
+        if (line[0] == '#') {
+            // eat all the comment lines
+            cout << line << endl;
+            continue;
+        }
+        
+        if (n == 0) {
+            // Magic Number
+            cout << "Magic Number " << line << endl;
+            
+        } else if (n == 1) {
+            // Width and Height
+            cout << "Width and Height " << line << endl;
+            iss >> width >> height;
+            size = width*height;
+            arr = new int[size];
+            
+        } else if (n == 2) {
+            // Max Value
+            cout << "Max Value " << line << endl;
+            iss >> max;
+            
+        } else {
+            // Image Values
+//            cout << '.';
+            int val = 0;
+            while (iss >> val && i < size) {
+                arr[i] = val;
+                ++i;
+            }
+        }
+        
+        ++n;
+    }
+    cout << endl;
+    
+    return true;
+}
+
+bool PGMImage::saveASCII(const char *file)
+{
+    ofstream outFile;
+    outFile.open(file);
+//    outFile << "Hello\n";
+    
+    outFile << "P2\n";
+    outFile << "# Created for Algorithms Project 2.\n";
+    outFile << width << ' ' << height << endl;
+    outFile << max << endl;
+    
+    for (int i=0; i < getSize(); ++i) {
+        outFile << arr[i] << "  ";
+        if ((i+1)%12==0)
+            outFile << endl;
+    }
+    
+    outFile.close();
+    return true;
+}
+
+bool PGMImage::loadBinary(const char *file)
+{
+    return true;
+}
+
+bool PGMImage::saveBinary(<#const char *file#>)
+{
+    return true;
+}
 
 #endif /* PGMImage_h */
--- pgm_compressor/main.cpp
+++ pgm_compressor/main.cpp
@@ -11,16 +11,34 @@
 
 using namespace std;
 
+void option1(const char* file);
+
 int main(int argc, const char * argv[]) {
     
-    if (argc >= 3 && strncmp (argv[1], "1", 2) == 0)
+    if (argc >= 3 && strncmp (argv[1], "1", 2) == 0) {
         cout << "Option 1...\n";
-    else if (argc >= 3 && strncmp (argv[1], "1", 2) == 0)
+        option1(argv[2]);
+    }
+    else if (argc >= 3 && strncmp (argv[1], "2", 2) == 0) {
         cout << "Option 2...\n";
-    else if (argc >= 3 && strncmp (argv[1], "1", 2) == 0)
+    }
+    else if (argc >= 3 && strncmp (argv[1], "3", 2) == 0) {
         cout << "Option 3...\n";
-    else if (argc >= 3 && strncmp (argv[1], "1", 2) == 0)
+    }
+    else if (argc >= 3 && strncmp (argv[1], "4", 2) == 0) {
         cout << "Option 4...\n";
+    }
+    else {
+        cout << "Invalid arguments\n";
+    }
     
     return 0;
+}
+
+void option1(const char* file) {
+    cout << file << endl;
+    PGMImage i;
+    i.loadASCII(file);
+    string newFile = "test.pgm";
+    i.saveASCII(newFile.c_str());
 }
